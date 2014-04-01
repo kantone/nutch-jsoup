c081008039bf5ae6c4e80104a0399fa5af959648
alexbit <alexbit.bit@gmail.com>
2014-04-01 16:32:12 +0200

    Create project

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 nutch-jsoup
 ===========
-
 Precise data extraction with nutch and Jsoup css selector via xml configuration
-Add custom field for each specific data that you need
 
+- Extract data with super easy jsoup selector ( jquery like )
+```sh
+Example : #nodeId tr:nth-child(2) td:nth-child(2) p 
+```
 
+- Add custom field for each specific data that you need
+- ElasticSearch query integration
 
+Intallation
+----
+1. Copy "plugin/index-domsjoup" under plugin directory
+2. Copy "conf/domjsoupconf.xml" under conf directory
+3. Copy and edit "conf/example.xml" under conf directory
 
+Configuration
+----
+To-do
+
+How to test
+----
+To-do
+
+Dependencies
+----
+jsoup-1.7.3.jar
+tagsoup-1.2.jar
---
+++ org/apache/nutch/indexer/domjsoup/DomJsoupIndexingFilter.java
@@ -0,0 +1,321 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.indexer.domjsoup;
+
+
+import java.io.File;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.indexer.IndexingException;
+import org.apache.nutch.indexer.IndexingFilter;
+import org.apache.nutch.indexer.NutchDocument;
+import org.apache.nutch.indexer.domjsoup.conf.Rules;
+import org.apache.nutch.indexer.domjsoup.rule.Parse.Fields.Elastic;
+import org.apache.nutch.indexer.domjsoup.rule.Parse.Fields.TextProcess.Replace;
+import org.apache.nutch.indexer.domjsoup.rule.Parse.Fields.TextProcess.Split;
+import org.apache.nutch.indexer.domjsoup.rule.Parse.Fields.TextProcess.Trim;
+import org.apache.nutch.storage.WebPage;
+import org.apache.nutch.storage.WebPage.Field;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.mortbay.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Jsoup dom extractor field extractor for nutch
+ * 
+ * @author Alex Kantone - @aKantone
+ * 
+ */
+
+public class DomJsoupIndexingFilter implements IndexingFilter {
+  public static final Logger LOG = LoggerFactory.getLogger(DomJsoupIndexingFilter.class);
+  public static final String conffileName = "domjsoupconf.xml";
+  private String conffile;
+  private static Collection<WebPage.Field> FIELDS = new HashSet<WebPage.Field>();
+  private org.apache.nutch.indexer.domjsoup.rule.Parse parse  = null;
+  private org.apache.nutch.indexer.domjsoup.conf.Rules.Rule rule = null;
+  private org.apache.nutch.indexer.domjsoup.conf.Rules.ElasticInfo elasticInfo = null;
+  @Override
+  public NutchDocument filter(NutchDocument doc, String url, WebPage page) throws IndexingException {
+	  URL u  = this.conf.getResource(conffileName);
+	  this.conffile = u.getPath();
+	 
+	  ByteBuffer k = page.getContent();
+	  String html = new String(k.array(),Charset.forName("UTF-8"));
+	  
+	  setXpathsRuleFile(url);
+	  if(this.rule != null){
+		  
+		  if(this.rule.isAddhtmlsourcefield()){			
+			  doc.add("html", html);
+		  }
+		  
+		  for (org.apache.nutch.indexer.domjsoup.rule.Parse.Fields entry : this.parse.getFields()) {
+			
+			  boolean processStandard = true;
+			  
+			  //ElasticCase
+			  Elastic elastic = entry.getElastic();
+			  if(elastic != null){
+				  if(elastic.isFindintoelastic()){
+					  processStandard = false;
+					  
+					  //Set this field as elasticSearch
+					  if(elastic.getElasticprocesstype().equals("setFieldValue")){
+						  String val = this.elasticQueryByUrl(elastic.getFindUrlValue(),this.elasticInfo.getClusterName(),this.elasticInfo.getHostNameOrIp(),this.elasticInfo.getElasticPort(),this.elasticInfo.getIndex(),elastic.getFieldName());
+						  Log.info("Setting " + entry.getFieldname() + " from elasticSearch query result");
+						  doc.add(entry.getFieldname(), val);
+					  }
+					  if(elastic.getElasticprocesstype().equals("processFieldJsoup")){
+						  String htmlFromElastic = this.elasticQueryByUrl(elastic.getFindUrlValue(),this.elasticInfo.getClusterName(),this.elasticInfo.getHostNameOrIp(),this.elasticInfo.getElasticPort(),this.elasticInfo.getIndex(),elastic.getFieldName());
+						  if(htmlFromElastic != null && !htmlFromElastic.isEmpty()){
+							  Log.info("Find html into elasticSearch and parse it with this rule");
+							  String val = "";
+							  Elements el = this.parse(htmlFromElastic, entry.getJsoupquery());
+							  if(el != null){
+								  val= parseRule(el,entry);
+							  }
+							  doc.add(entry.getFieldname(), val);
+						  }
+						  else {
+							  Log.warn("Cannot find html value from elasticsearch query");
+							  doc.add(entry.getFieldname(), "");
+						  }
+					  }
+				  }
+			  }
+			  
+			  if(processStandard){
+				  String val = "";
+				  Elements el = this.parse(html, entry.getJsoupquery());
+				  if(el != null){
+					  val= parseRule(el,entry);
+				  }
+				  doc.add(entry.getFieldname(), val);
+			  }
+		  }
+	  }
+	  
+	 
+	  return doc;
+  }
+  
+  /**
+   * Query ElasticSearch for find field value or html to process
+   * @param url
+   * @param clusterName
+   * @param hostNameOrIp
+   * @param hostPortOrIp
+   * @param index
+   * @param fieldAsString
+   * @return
+   */
+  private String elasticQueryByUrl(String url,String clusterName,String hostNameOrIp,int hostPort,String index,String fieldAsString){	
+	  Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", clusterName).build();
+	  TransportClient client = new TransportClient(settings);
+	  client.addTransportAddress(new InetSocketTransportAddress(hostNameOrIp, hostPort));
+	  QueryBuilder q =QueryBuilders.commonTerms("url", url);
+	 
+	  SearchResponse response = client.prepareSearch(index)
+			  .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+			  .setQuery(q)
+			  .setFrom(0).setSize(1)
+			  .setExplain(true)
+			  .execute().actionGet();
+	  	 
+	  SearchHits hits = response.getHits();
+	  if(hits.totalHits() >= 1){				
+		  SearchHit hit = hits.getAt(0);
+		  Map<String, Object> s = hit.sourceAsMap();
+		  String o =(String) s.get(fieldAsString);		
+		  return o;
+	  }
+	  return null;
+  }
+  
+  
+  /***
+   * Process rule xml 
+   * @param el
+   * @param rule
+   * @return
+   */
+  private String parseRule(Elements el,org.apache.nutch.indexer.domjsoup.rule.Parse.Fields rule){	  
+	  String val = "";
+	  
+	  //set value
+	  if(rule.getReturnType().equals("text"))
+		  val = el.text();
+	  else if (rule.getReturnType().equals("html"))
+		  val = el.html();
+	  else if (rule.getReturnType().equals("attr"))
+		  val = el.attr(rule.getAttrname());
+	  
+	  org.apache.nutch.indexer.domjsoup.rule.Parse.Fields.TextProcess textProcess =  rule.getTextProcess();	  
+	  if(textProcess != null){
+		  //remove lines?
+		  
+		  //Replace
+		  List<Replace> repl = textProcess.getReplace();
+		  if(repl != null){
+			  for(Replace r : repl){
+				  val = val.replace(r.getFindstring(), r.getVal());
+			  }
+		  }
+		  
+		  //Split
+		  List<Split> split = textProcess.getSplit();
+		  if(split != null){
+			  for(Split s :  split){
+				  String[] vals = val.split(s.getSplitvalue());
+				  val = vals[s.getReturnindex()];
+			  }
+		  }
+		  
+		//post process text
+		  //Trim
+		  List<Trim> trim = textProcess.getTrim();
+		  if(split != null){
+			  for(Trim t : trim){
+				  if(t.getType().equals("left"))
+					  val = val.replaceAll("^" + t.getTrimvalue(), "");
+				  else if(t.getType().equals("right"))
+					  val = val.replaceAll( t.getTrimvalue() + "$","");
+				  else if(t.getType().equals("both")){
+					  val = val.replaceAll("^" + t.getTrimvalue(), "");
+					  val = val.replaceAll( t.getTrimvalue() + "$","");
+				  }		 
+			  }
+		  }
+	  
+	  }
+	  return val;
+  }
+  
+  
+  /**
+   * Perform xpath
+   * @param html
+   */
+  private Elements parse(String html,String query){
+		//My Xpath
+		  
+		    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		    factory.setNamespaceAware(false);
+		    factory.setValidating(false);
+		    try {	    		
+		    	
+		    	org.jsoup.nodes.Document doc2  = Jsoup.parse(html);		    	
+		    	Elements viewedEl = doc2.select(query);
+		    	return viewedEl;
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		  
+	  }
+
+  /**
+   * Read global xpath configuration file and return the right xpath rules xml files by url contains filter
+   * @return
+   */
+  private void setXpathsRuleFile(String url){
+	  try {
+		  File f = new File(conffile);
+		  if(f.exists()){
+			  JAXBContext jaxbContext = JAXBContext.newInstance(Rules.class);
+			  Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();		  
+			  Rules config = (Rules) jaxbUnmarshaller.unmarshal(f);
+			  this.elasticInfo = config.getElasticInfo();
+			  
+			  for (org.apache.nutch.indexer.domjsoup.conf.Rules.Rule entry : config.getRule()) {
+					
+				  this.rule = entry;
+				  
+				  //Perform Xpath here
+				  if(url.contains(entry.getUrlcontain())){
+					  File f2 = new File(entry.getFile());
+					  if(f2.exists()){
+						  jaxbContext = JAXBContext.newInstance( org.apache.nutch.indexer.domjsoup.rule.Parse.class);
+						  jaxbUnmarshaller = jaxbContext.createUnmarshaller();		  
+						  this.parse = (org.apache.nutch.indexer.domjsoup.rule.Parse) jaxbUnmarshaller.unmarshal(f2);
+						  return;
+					  }
+					  else{						  
+						  LOG.error("File not exist : " + entry.getFile());
+					  }
+				  }	  
+			  }		  
+		  }
+		  else{
+			  LOG.error("File not exist : " + conffile);
+		  }
+		  return;
+		  
+
+	} catch (JAXBException e) {
+		e.printStackTrace();
+		return;
+	}	  
+  }
+    
+
+  private Configuration conf; 
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
+
+  public Configuration getConf() {
+    return this.conf;
+  }
+
+  @Override
+  public Collection<Field> getFields() {
+    return FIELDS;
+  }
+  
+  public static void main(String[] args) throws Exception {
+	  System.out.println("DomJsoupIndexingFilter plugin");
+}
+
+
+}
